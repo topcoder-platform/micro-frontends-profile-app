7a501b069acf1303521b5e688db379aeb42d3b65
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-02-16 18:21:21 +0530

    QA changes

---
+++ config/qa.js
@@ -0,0 +1,10 @@
+module.exports = {
+  API: {
+    V5: "https://api.topcoder-qa.com/v5",
+    V3: "https://api.topcoder-qa.com/v3",
+  },
+  URL: {
+    FORUMS: "https://apps.topcoder-qa.com/forums",
+    COMMUNITY: "https://community.topcoder-qa.com",
+  },
+};
